922678a2dc9ebf3c31687162b3262843aba68968
Dave Mastropolo <davemastropolo@Margarets-MacBook-Air.local>
2023-04-07 18:25:29 -0700

    App.js test2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           add something back out a commit Edit <code>src/App.js</code> and save to reload.
+           test2 add something back out a commit Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
